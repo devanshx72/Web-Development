359644084a8ad96027637947b711dbbd2b9a2bce
Devansh Sharma <143217931+devanshx72@users.noreply.github.com>
2025-11-18 15:32:13 +0530

    Variables and Data Types

---
+++ JS_01/1_variables.js
@@ -0,0 +1,6 @@
+const accounntId = 123456
+let accountEmail = "abc@mail.com"
+var accountPassword = "password123"
+accountCity = "New York"
+
+// accountId = 654321 // This will cause an error because accounntId is a constant
---
+++ JS_01/2_symbol.js
@@ -0,0 +1,18 @@
+let name = "John Doe"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power of 53
+// bigint => larger than 2 to the power of 53
+// string => "Hello World"
+// boolean => true/false
+// null => standalone value that represents "no value"
+// undefined => variable that is declared but not assigned a value
+// symbol => unique identifiers
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
