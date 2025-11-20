2488c4133125f300ff9d1f8e6d451edbbf98a6b9
Devansh Sharma <143217931+devanshx72@users.noreply.github.com>
2025-11-20 11:49:44 +0530

    Operations done

--- JS_01/3_conversionOperations.js
+++ JS_01/3_conversionOperations.js
@@ -13,3 +13,33 @@ console.log(valueInNumber);
 // false -> 0
 // null -> 0
 // undefined -> NaN
+
+// *********** Operations ****************
+console.log("Operations--------------------");
+
+let value = 33
+let negValue = -value;
+// console.log(negValue);
+// hi = -hi; // Error: hi is not defined
+
+
+// let str1 = "John";
+// let str2 = "Doe";
+
+// let fullName = str1 + " " + str2;
+// console.log(fullName);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 1 + "1");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+let n1, n2, n3;
+n1 = n2 = n3 = 2 + 2;
+
+console.log(n1, n2, n3);
+n2 += 5; 
+console.log(n1, n2, n3);
