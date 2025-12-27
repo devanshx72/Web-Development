de64bb8f9c75b3e07b4f199696aefd0b93580b77
Devansh Sharma <143217931+devanshx72@users.noreply.github.com>
2025-12-28 03:24:26 +0530

    stack vs heap memory

--- JS_01/datatypes-summary.js
+++ JS_01/datatypes-summary.js
@@ -30,3 +30,35 @@ const myFunction = function(){
 console.log(typeof str); // "string"
 
 // typeof tells the datatype of the variable
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack vs Heap Memory
+
+// Primitive data types are stored in Stack memory
+// Reference data types (Non-primitive) are stored in Heap memory
+
+let myName = "Devansh Sharma" // stored in Stack
+let otherName = myName;
+console.log(myName);
+
+otherName = "John Doe";
+
+console.log(myName);
+
+let user1 = {
+    name: "Devansh Sharma",
+    age: 20
+} // stored in Heap
+
+let user2 = user1; // user2 stores the reference to the object in Heap
+
+console.log(user1.name); // Devansh Sharma
+
+user2.name = "John Doe"; // Modifying the object via user2 reference
+
+console.log(user1.name);
+console.log(user2.name);
